b4f8f251b02f24263a0c52e8bc047296efe60abb
sabler <gizmo.00bases@icloud.com>
2024-05-19 17:35:47 -0700

    Done with streaming and suspense

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,16 +6,18 @@ import { fetchCardData } from "../../lib/data";
 import { Suspense } from "react";
 import { RevenueChartSkeleton } from "@/app/ui/skeletons";
 import { LatestInvoicesSkeleton } from "@/app/ui/skeletons";
+import CardWrapper from "../../ui/dashboard/cards";
+import { CardsSkeleton } from "@/app/ui/skeletons";
 
 
 export default async function Page() {
     
-    const {
-        numberOfInvoices,
-        numberOfCustomers,
-        totalPaidInvoices,
-        totalPendingInvoices
-    } = await fetchCardData();
+    // const {
+    //     numberOfInvoices,
+    //     numberOfCustomers,
+    //     totalPaidInvoices,
+    //     totalPendingInvoices
+    // } = await fetchCardData();
     
     return (
 
@@ -24,16 +26,22 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+
+      <Suspense fallback={<CardsSkeleton />}>
+        <CardWrapper />
+        </Suspense>
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        />
+        /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+
+
       <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
